d7bdf9929c21ec04f173175624edcc53a4d83634
nehoraii <nehoraii555@gmail.com>
2023-05-24 23:01:38 +0300

    create the server

--- src/main/java/com/example/gmailserver/controller/SendGmail.java
+++ src/main/java/com/example/gmailserver/controller/SendGmail.java
@@ -1,21 +1,20 @@
 package com.example.gmailserver.controller;
-
-import com.example.gmailserver.functionGmail.EmailSender;
 import com.example.gmailserver.functionGmail.GmailQuickStart;
 import com.example.gmailserver.vo.GmailVo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Validated
 @RestController
 @RequestMapping("/sendGmail")
 public class SendGmail {
-    @GetMapping("/send")
-    public void sendGmail(@RequestBody GmailVo gmailVo){
+    @PostMapping("/send")
+    public ResponseEntity<String> sendGmail(@RequestBody GmailVo gmailVo){
         GmailQuickStart.sendGmail(gmailVo.getSendTo(),gmailVo.getMassage());
+        String jsonResponse = "GOOD!!!";
+        return ResponseEntity.status(HttpStatus.OK).body(jsonResponse);
 
     }
 }
